676d0938ff644fea491d34502385fb65ef10d8c6
nooralam <mdnooralam66266@gmail.com>
2024-11-10 19:43:39 +0530

    new commit

--- src/Comman.jsx
+++ src/Comman.jsx
@@ -9,7 +9,7 @@ const Comman = (props) => {
   <div className="container-fluid nav bg">
         <div className="row">
              <div className="col-10 max-auto">
-              <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+              <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-row">
                 <div className="row">
                 <h1> 
                    {props.name}
@@ -24,8 +24,8 @@ const Comman = (props) => {
                         </NavLink>
                     </div>
               </div>
-              <div className="col-lg-6 order-1 order-lg-2 header-img">
-                <img src={props.imgsrc} className="img-fluid animated" alt="home img" />
+              <div className="col-lg-6 order-2 order-lg-2 header-img " >
+                <img src={props.imgsrc} className="img-fluid animated" alt="home img" width="auto" height="auto" />
 
               </div>
               </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
             <div className="col-10 max-auto">
 
 
-       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
+       <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+  
     <NavLink className="navbar-brand" to="/">Navbar</NavLink>
     {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button> */}
-    </div>
+    
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
         <li className="nav-item">
